d7f82c267ab7d4e8d9a645d095b2644eca23f527
geo <geoboiffils34@gmail.com>
2021-05-05 13:09:43 +0200

    this is the end

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -9,36 +9,36 @@ const bcrypt = require('bcryptjs');
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  const passWord = await User.findOne({ password });
-  if (user && passWord) {
+  if(!user) return res.status(400).send("Invalid email");
+  const comparePassword = await bcrypt.compare(req.body.password, user.password);
+  if (!comparePassword){
+    return res.status(400).send("Invalid password");
+  } else if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email.toLowerCase(),
       token: generateToken(user._id),
     });
-  } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
+  } 
 });
 
 // @desc Register a new user
 // @route Post /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email } = req.body;
   const userExist = await User.findOne({ email });
   if (userExist) {
     res.status(400);
     throw new Error("L'utilisateur existe déjà");
   }
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt); 
+  const password = await bcrypt.hash(req.body.password, salt); 
   const user = await User.create({
     name,
     email,
-    hashedPassword,
+    password,
   });
 
   if (user) {
